a1d81caa4602ca50445a75ca3886c6ee930f9792
Shreyas Mishra <shreyasmishrasm048@gmail.com>
2025-01-02 12:41:23 +0530

    Minor Update- Persist State( reload)

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux"; // Import Provider
-import store from "./redux/store"; // Import your Redux store
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
 // Import Pages
@@ -16,32 +16,34 @@ import OwnerLogin from "./pages/ownerLog";
 import Cart from "./pages/CartPage";
 import AddMenu from "./pages/addMenu";
 import ProfilePage from "./pages/ProfilePage";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {" "}
-      {/* Wrap the entire app with Redux Provider */}
-      <BrowserRouter>
-        <Routes>
-          {/* Common routes */}
-          <Route path="" element={<RegisterPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="ownerL" element={<OwnerLogin />} />
-          <Route path="ownerR" element={<OwnerReg />} />
-          {/* Student routes */}
-          <Route path="home" element={<HomePage />} />
-          <Route path="canteen" element={<CanteenPage />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="getMenu/:canteenId" element={<CanteenPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          {/* Owner routes */}
-          <Route path="ownerU" element={<AddCanteen />} />
-          <Route path="menuUpload/:canteenId" element={<AddMenu />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        {" "}
+        <BrowserRouter>
+          <Routes>
+            {/* Common routes */}
+            <Route path="" element={<RegisterPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="ownerL" element={<OwnerLogin />} />
+            <Route path="ownerR" element={<OwnerReg />} />
+            {/* Student routes */}
+            <Route path="home" element={<HomePage />} />
+            <Route path="canteen" element={<CanteenPage />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="getMenu/:canteenId" element={<CanteenPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            {/* Owner routes */}
+            <Route path="ownerU" element={<AddCanteen />} />
+            <Route path="menuUpload/:canteenId" element={<AddMenu />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./reducers/cartReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedCartReducer,
   },
 });
 
+export const persistor = persistStore(store);
 export default store;
