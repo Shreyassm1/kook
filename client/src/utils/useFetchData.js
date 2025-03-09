e90f42aea559be4120dc87a9caea776ee580d354
Shreyas Mishra <shreyasmishrasm048@gmail.com>
2025-03-10 00:50:04 +0530

    withcreds check2

--- client/src/utils/useFetchData.js
+++ client/src/utils/useFetchData.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+axios.defaults.withCredentials = true;
 
 const useFetchData = (url) => {
   const [data, setData] = useState([]);
